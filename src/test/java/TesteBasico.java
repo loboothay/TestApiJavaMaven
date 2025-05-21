052bb32b1c80043504d1948916329b5c0a5a62a4
Thaynara Lobo <loboo.thaynara@gmail.com>
2025-05-21 16:05:14 -0300

    Adicionando teste de listar dados por ID

--- src/test/java/TesteBasico.java
+++ src/test/java/TesteBasico.java
@@ -1,7 +1,8 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 
 public class TesteBasico {
     // Define um metodo de teste
@@ -18,5 +19,25 @@ public class TesteBasico {
                 .then() // Define as validações da resposta
                 .statusCode(200) // Verifica se o status code da resposta é 200 (OK)
                 .log().all();    // Loga no console todos os detalhes da resposta (body, headers, etc.)
+    }
+
+    @Test
+    public void testGetBookingId() {
+
+        RestAssured.baseURI = "https://restful-booker.herokuapp.com";
+
+        given()
+                .header("Accept", "application/json")
+                .when()
+                .get("/booking/595")
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo("John"))
+                .body("lastname", equalTo("Smith"))
+                .body("totalprice", equalTo(111))
+                .body("depositpaid", is(true))
+                .body("bookingdates.checkin", equalTo("2018-01-01"))
+                .body("bookingdates.checkout", equalTo("2019-01-01"))
+                .body("additionalneeds", equalTo("Breakfast"));
     }
 }
